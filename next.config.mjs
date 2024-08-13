925a7dd943b66116965240a76f9e8f6f2aa1f66f
Nyasha Musanhu <danielsnyasha@outlook.com>
2024-08-13 13:27:18 +0200

    configurator page

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+      domains: ['utfs.io', 'pixabay.com', 'unsplash.com', 'localhost'],
+    },
+  }
 
-export default nextConfig;
+  export default nextConfig
